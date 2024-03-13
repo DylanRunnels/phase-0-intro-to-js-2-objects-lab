fc592f8e07714c53f52bba88315dee2a3377287e
Dylan Runnels <runnels.dylan@yahoo.com>
2024-03-12 21:08:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+let employee = {
+    name: "Hailey Green",
+    streetAddress: "12 Banks Dr"
+  }
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let clone = Object.assign({}, employee);
+    clone[key] = value;
+    return clone;
+  }
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Banks Dr");
+console.log(updatedEmployee);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function  deleteFromEmployeeByKey(employee, key){
+    let clone = Object.assign({}, employee);
+    delete clone[key];
+    return clone;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
